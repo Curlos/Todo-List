d252cba20c2f5524895e40bb30b31c579a55c7cf
Carlos Martinez <curlosmart@gmail.com>
2021-08-12 14:18:50 -0400

    applied eslint config to js file

--- src/index.js
+++ src/index.js
@@ -1,99 +1,100 @@
-const taskItemList = document.querySelector('.taskItemList')
-const taskInput = document.querySelector('.taskInput')
-const addTaskButtons = document.querySelectorAll('.addTaskButton')
-const modal = document.querySelector('.modal')
+const taskItemList = document.querySelector('.taskItemList');
+const taskInput = document.querySelector('.taskInput');
+const addTaskButtons = document.querySelectorAll('.addTaskButton');
+const modal = document.querySelector('.modal');
 
-const allTasks = []
-const taskID = 0
+const allTasks = [];
+const taskID = 0;
 class TodoItem {
-
-    constructor(name, complete=false, description='', dueDate='', listType='', priority='') {
-        this.name = name
-        this.complete = complete
-        this.description = description
-        this.dueDate = dueDate
-        this.listType = listType
-        this.priority = priority
-    }
+  constructor(name, complete = false, description = '', dueDate = '', listType = '', priority = '') {
+    this.name = name;
+    this.complete = complete;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.listType = listType;
+    this.priority = priority;
+  }
 
     setName = (name) => {
-        this.name = name
+      this.name = name;
     }
 
     setDescription = (description) => {
-        this.description = description
+      this.description = description;
     }
 
     setDueDate = (dueDate) => {
-        this.dueDate = dueDate
+      this.dueDate = dueDate;
     }
 
     setListType = (listType) => {
-        this.listType = listType
+      this.listType = listType;
     }
 
     setPriority = (priority) => {
-        this.priority = priority
+      this.priority = priority;
     }
 
     toggleComplete = () => {
-        this.complete = !complete
+      this.complete = !complete;
     }
 
     getName = () => this.name
+
     getDescription = () => this.description
+
     getDueDate = () => this.dateDue
+
     getListType = () => this.listType
+
     getPriority = () => this.priority
+
     isComplete = () => this.complete
 }
 
 const editTodoItem = () => {
 
-}
+};
 
 // Logic for adding task
 const addTask = (event) => {
-    event.preventDefault()
-    //displayAddTaskForm()
-    const taskName = taskInput.value
-    const taskItem = new TodoItem(taskName, false)
-    allTasks.push(taskItem)
+  event.preventDefault();
+  // displayAddTaskForm()
+  const taskName = taskInput.value;
+  const taskItem = new TodoItem(taskName, false);
+  allTasks.push(taskItem);
 
-    displayNewTask(taskName)
-    taskInput.value = ''
+  displayNewTask(taskName);
+  taskInput.value = '';
 
-    displayAllTasks()
-    console.log(allTasks)
-    displayAddTaskForm()
-}
+  displayAllTasks();
+  console.log(allTasks);
+  displayAddTaskForm();
+};
 
 const displayAddTaskForm = () => {
-    const addTaskForm = document.createElement('div')
-    addTaskForm.classList.add('addTaskForm')
+  const addTaskForm = document.createElement('div');
+  addTaskForm.classList.add('addTaskForm');
 
-    const formChildElems = document.createElement('div')
-    formChildElems.classList.add('formChildElems')
+  const formChildElems = document.createElement('div');
+  formChildElems.classList.add('formChildElems');
 
-    const taskNameInput = document.createElement('input')
-    taskNameInput.setAttribute('type', 'text')
-    taskNameInput.setAttribute('id', 'taskNameInput')
-    taskNameInput.setAttribute('placeholder', 'Task name')
-    taskNameInput.classList.add('taskNameInput')
-    taskNameInput.classList.add('taskInput')
+  const taskNameInput = document.createElement('input');
+  taskNameInput.setAttribute('type', 'text');
+  taskNameInput.setAttribute('id', 'taskNameInput');
+  taskNameInput.setAttribute('placeholder', 'Task name');
+  taskNameInput.classList.add('taskNameInput');
+  taskNameInput.classList.add('taskInput');
 
-    const taskDescriptionInput = document.createElement('input')
-    taskDescriptionInput.setAttribute('type', 'text')
-    taskDescriptionInput.setAttribute('id', 'taskDescriptionInput')
-    taskDescriptionInput.setAttribute('placeholder', 'Description')
+  const taskDescriptionInput = document.createElement('input');
+  taskDescriptionInput.setAttribute('type', 'text');
+  taskDescriptionInput.setAttribute('id', 'taskDescriptionInput');
+  taskDescriptionInput.setAttribute('placeholder', 'Description');
 
-    formChildElems.append(taskNameInput)
-    formChildElems.append(taskDescriptionInput)
-    addTaskForm.append(formChildElems)
-
-}
-
-
+  formChildElems.append(taskNameInput);
+  formChildElems.append(taskDescriptionInput);
+  addTaskForm.append(formChildElems);
+};
 
 /*
 
@@ -109,86 +110,85 @@ const displayAddTaskForm = () => {
 
 // DOM manipulation for displaying a newly added task
 const displayNewTask = (taskName) => {
-    const newTaskElem = document.createElement('div')
-    const taskNameText = document.createTextNode(taskName)
+  const newTaskElem = document.createElement('div');
+  const taskNameText = document.createTextNode(taskName);
 
-    const checkBoxInput = document.createElement('input')
-    checkBoxInput.setAttribute('type', 'checkbox')
-    checkBoxInput.addEventListener('click', toggleCompleteTask)
+  const checkBoxInput = document.createElement('input');
+  checkBoxInput.setAttribute('type', 'checkbox');
+  checkBoxInput.addEventListener('click', toggleCompleteTask);
 
-    const editIcon = document.createElement('i')
-    editIcon.classList.add('fas')
-    editIcon.classList.add('fa-edit')
+  const editIcon = document.createElement('i');
+  editIcon.classList.add('fas');
+  editIcon.classList.add('fa-edit');
 
-    const taskLeftSide = document.createElement('span')
-    taskLeftSide.classList.add('taskLeftSide')
+  const taskLeftSide = document.createElement('span');
+  taskLeftSide.classList.add('taskLeftSide');
 
-    const taskRightSide = document.createElement('span')
-    taskRightSide.classList.add('taskRightSide')
+  const taskRightSide = document.createElement('span');
+  taskRightSide.classList.add('taskRightSide');
 
-    taskLeftSide.append(checkBoxInput)
-    taskLeftSide.append(taskNameText)
-    taskRightSide.append(editIcon)
+  taskLeftSide.append(checkBoxInput);
+  taskLeftSide.append(taskNameText);
+  taskRightSide.append(editIcon);
 
-    newTaskElem.classList.add('taskItem')
-    newTaskElem.append(taskLeftSide)
-    newTaskElem.append(taskRightSide)
+  newTaskElem.classList.add('taskItem');
+  newTaskElem.append(taskLeftSide);
+  newTaskElem.append(taskRightSide);
 
-    newTaskElem.addEventListener('click', toggleModal)
-    taskItemList.append(newTaskElem)
-
-}
+  newTaskElem.addEventListener('click', toggleModal);
+  taskItemList.append(newTaskElem);
+};
 
 const toggleCompleteTask = (event) => {
-    console.log(event.target.parentElement)
-}
+  console.log(event.target.parentElement);
+};
 
 const displayCompleteTask = () => {
 
-}
+};
 
 const displayAllTasks = () => {
-    for (let task of allTasks) {
-        console.log(task.getName())
-        console.log(task)
-    }
-}
+  for (const task of allTasks) {
+    console.log(task.getName());
+    console.log(task);
+  }
+};
 
 const generateTestTasks = () => {
-    const testTasks = ['display tasks from tasks array', 'style task when checked completed', 'Let users edit tasks to change their name', 'Let users edit tasks to change their priority']
+  const testTasks = ['display tasks from tasks array', 'style task when checked completed', 'Let users edit tasks to change their name', 'Let users edit tasks to change their priority'];
 
-    for (let testTask of testTasks) {
-        const taskItem = new TodoItem(testTask)
-        allTasks.push(taskItem)
-        displayNewTask(testTask)
-        allTasks.push(taskItem)
-    }
-}
+  for (const testTask of testTasks) {
+    const taskItem = new TodoItem(testTask);
+    allTasks.push(taskItem);
+    displayNewTask(testTask);
+    allTasks.push(taskItem);
+  }
+};
 
 const toggleModal = () => {
-    modal.classList.toggle('show-modal')
-    console.log('hello wrold')
-}
+  modal.classList.toggle('show-modal');
+  console.log('hello wrold');
+};
 
 const windowOnClick = (event) => {
-    if (event.target === modal) {
-        toggleModal()
-    } 
-}
+  if (event.target === modal) {
+    toggleModal();
+  }
+};
 
 for (button of addTaskButtons) {
-    button.addEventListener('click', addTask)
+  button.addEventListener('click', addTask);
 }
 
-const simpleTask = new TodoItem('title', false)
-console.log(simpleTask.getName())
-simpleTask.setName('newName bois')
-console.log(simpleTask.getName())
-console.log(simpleTask.isComplete())
+const simpleTask = new TodoItem('title', false);
+console.log(simpleTask.getName());
+simpleTask.setName('newName bois');
+console.log(simpleTask.getName());
+console.log(simpleTask.isComplete());
 
-console.log(simpleTask)
+console.log(simpleTask);
 
-taskInput.addEventListener('submit', addTask)
-window.addEventListener('click', windowOnClick)
+taskInput.addEventListener('submit', addTask);
+window.addEventListener('click', windowOnClick);
 
-generateTestTasks()
+generateTestTasks();
